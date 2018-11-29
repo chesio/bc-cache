caae81a13426b19cf67e3ff907d4d18c22824dcf
Česlav Przywara <cp@bluechip.at>
2018-11-29 10:16:15 +0100

    Restore develop as version number in develop branch.

--- bc-cache.php
+++ bc-cache.php
@@ -3,7 +3,7 @@
  * Plugin Name: BC Cache
  * Plugin URI: https://github.com/chesio/bc-cache
  * Description: Simple disk cache plugin inspired by Cachify.
- * Version: 1.2.1
+ * Version: develop
  * Author: Česlav Przywara <ceslav@przywara.cz>
  * Author URI: https://www.chesio.com
  * Requires PHP: 7.0
