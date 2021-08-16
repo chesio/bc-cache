c3bd4f2dc58774c8aa2010896f70c229b7c080ab
Česlav Przywara <cp@bluechip.at>
2021-08-16 13:29:38 +0200

    Bump "Tested up to" version to 5.7

--- bc-cache.php
+++ bc-cache.php
@@ -9,7 +9,7 @@
  * Author URI: https://www.chesio.com
  * Requires PHP: 7.2
  * Requires WP: 5.3
- * Tested up to: 5.5
+ * Tested up to: 5.7
  * Text Domain: bc-cache
  * GitHub Plugin URI: https://github.com/chesio/bc-cache
  */
