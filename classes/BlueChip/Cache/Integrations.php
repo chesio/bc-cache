chesio/bc-cache

commit 2ea17756c58e34296d01fb502845efec901ab7ae

Refactor integrations with 3rd party plugins into separate class

classes/BlueChip/Cache/Integrations.php
@@ -1,0 +1,79 @@
+<?php
+
+namespace BlueChip\Cache;
+
+/**
+ * Container for integrations with 3rt party plugins.
+ */
+abstract class Integrations
+{
+    public static function initialize()
+    {
+        // Integration with Autoptimize
+        if (defined('AUTOPTIMIZE_PLUGIN_VERSION')) {
+            add_filter(Hooks::FILTER_FLUSH_HOOKS, [self::class, 'flushOnAutoptimizePurge'], 10, 0);
+        }
+
+        // Integration with Yoast SEO
+        if (isset($GLOBALS['wpseo_sitemaps'])) {
+            // XML sitemaps feature in Yoast SEO is active.
+            add_filter(Hooks::FILTER_CACHE_WARM_UP_INITIAL_URL_LIST, [self::class, 'getUrlsFromYoastSeoSitemap'], 10, 0);
+        }
+    }
+
+
+    /**
+     * Add Autoptimize purge action to list of actions that trigger cache flush.
+     *
+     * @param array $hooks
+     *
+     * @return array
+     */
+    public static function flushOnAutoptimizePurge(array $hooks): array
+    {
+        $hooks['autoptimize_action_cachepurged'] = 100;
+
+        return $hooks;
+    }
+
+
+    /**
+     * @return string[]|null List of URLs from Yoast SEO sitemap or null if the sitemap is not activated.
+     */
+    public static function getUrlsFromYoastSeoSitemap(): ?array
+    {
+        if (!isset($GLOBALS['wpseo_sitemaps'])) {
+            // This sanity check is perhaps unnecessary, but this method is public after all.
+            return null;
+        }
+
+        $sitemap_providers = $GLOBALS['wpseo_sitemaps']->providers;
+
+        $entries_per_page = 1000; // This is default value used in Yoast SEO.
+
+        $url_sets = [];
+
+        foreach ($sitemap_providers as $sitemap_provider) {
+            // Types of sitemaps cannot be retrieved in any simpler way, we have to parse them from index links.
+            $index_links = $sitemap_provider->get_index_links($entries_per_page);
+
+            foreach ($index_links as ['loc' => $index_link]) {
+                $matches = [];
+
+                if (preg_match('/(\w+)\-sitemap(\d*)\.xml$/', $index_link, $matches) !== false) {
+                    $type = $matches[1];
+                    $page_num = $matches[2] ? (int) $matches[2] : 1;
+
+                    if ($sitemap_provider->handles_type($type)) {
+                        $url_sets[] = \array_column(
+                            $sitemap_provider->get_sitemap_links($type, $entries_per_page, $page_num),
+                            'loc'
+                        );
+                    }
+                }
+            }
+        }
+
+        return \array_merge(...$url_sets);
+    }
+}

classes/BlueChip/Cache/Feeder.php
@@ -108,66 +108,43 @@ class Feeder
     /**
      * Get list of URLs to crawl.
      *
-     * Internally, this method fetches URLs from XML sitemap providers - the following providers are supported:
-     *
-     * 1. Yoast SEO - XML sitemaps have to be turned on in plugin settings.
-     *
-     * If no provider in the list above is available, WordPress core XML sitemap providers are used as fallback method.
+     * Internally, this method fetches URLs from core XML sitemap providers, but this handling can be shortcut
+     * via `bc-cache/filter:cache-warm-up-initial-url-list` filter.
      *
      * @return string[] List of URLs to crawl
      */
     private function getUrls(): array
     {
-        $urls = [];
+        $urls = apply_filters(Hooks::FILTER_CACHE_WARM_UP_INITIAL_URL_LIST, null);
 
-        if (isset($GLOBALS['wpseo_sitemaps'])) {
-            // XML sitemaps feature in Yoast SEO is active.
+        if ($urls === null) {
+            // If no URLs are provided by other means, sse XML sitemaps providers from WordPress core.
 
-            $sitemap_providers = $GLOBALS['wpseo_sitemaps']->providers;
-
-            $entries_per_page = 1000; // This is default value used in Yoast SEO.
-
-            foreach ($sitemap_providers as $sitemap_provider) {
-                // Types of sitemaps cannot be retrieved in any simpler way, we have to parse them from index links.
-                $index_links = $sitemap_provider->get_index_links($entries_per_page);
-
-                foreach ($index_links as ['loc' => $index_link]) {
-                    $matches = [];
-
-                    if (preg_match('/(\w+)\-sitemap(\d*)\.xml$/', $index_link, $matches) !== false) {
-                        $type = $matches[1];
-                        $page_num = $matches[2] ? (int) $matches[2] : 1;
-
-                        if ($sitemap_provider->handles_type($type)) {
-                            $urls[] = \array_column(
-                                $sitemap_provider->get_sitemap_links($type, $entries_per_page, $page_num),
-                                'loc'
-                            );
-                        }
-                    }
-                }
-            }
-        } else {
-            // Use XML sitemaps providers from WordPress core.
-
-            /** @var \WP_Sitemaps_Provider[] */
+            /** @var \WP_Sitemaps_Provider[] $sitemap_providers */
             $sitemap_providers = wp_get_sitemap_providers();
+
+            $url_sets = [];
 
             foreach ($sitemap_providers as $sitemap_provider) {
                 foreach (\array_keys($sitemap_provider->get_object_subtypes()) as $object_subtype) {
                     $max_num_pages = $sitemap_provider->get_max_num_pages($object_subtype);
                     for ($page_num = 1; $page_num <= $max_num_pages; ++$page_num) {
-                        $urls[] = \array_column(
+                        $url_sets[] = \array_column(
                             $sitemap_provider->get_url_list($page_num, $object_subtype),
                             'loc'
                         );
                     }
                 }
             }
+
+            $urls = \array_merge(...$url_sets);
         }
 
         // Prepend home URL to the merged list of all URLs as it arguably represents the most important page on website.
-        return \array_unique(apply_filters(Hooks::FILTER_CACHE_WARM_UP_URL_LIST, \array_merge([home_url('/'),], ...$urls)));
+        \array_unshift($urls, home_url('/'));
+
+        // Make sure only unique URLs are returned.
+        return \array_unique(apply_filters(Hooks::FILTER_CACHE_WARM_UP_FINAL_URL_LIST, $urls));
     }
 
 

classes/BlueChip/Cache/Hooks.php
@@ -100,9 +100,14 @@ abstract class Hooks
     public const FILTER_CACHE_WARM_UP_RUN_TIMEOUT = 'bc-cache/filter:cache-warm-up-run-timeout';
 
     /**
-     * @var string Name of hook to filter list of URLs to be fetched to cache warm up.
+     * @var string Name of hook to filter initial list of URLs to be processed to cache warm up.
      */
-    public const FILTER_CACHE_WARM_UP_URL_LIST = 'bc-cache/filter:cache-warm-url-list';
+    public const FILTER_CACHE_WARM_UP_INITIAL_URL_LIST = 'bc-cache/filter:cache-warm-up-initial-url-list';
+
+    /**
+     * @var string Name of hook to filter final list of URLs to be processed in cache warm up.
+     */
+    public const FILTER_CACHE_WARM_UP_FINAL_URL_LIST = 'bc-cache/filter:cache-warm-up-final-url-list';
 
     /**
      * @var string Name of hook to filter list of whitelisted query string arguments.

classes/BlueChip/Cache/Plugin.php
@@ -208,6 +208,9 @@ class Plugin
      */
     public function init(): void
     {
+        // Activate integrations with 3rd party plugins - must be done early in this method!
+        Integrations::initialize();
+
         // Add Disallow section to robots.txt.
         add_filter('robots_txt', [$this, 'alterRobotsTxt'], 10, 1);
 
@@ -215,9 +218,6 @@ class Plugin
         foreach (apply_filters(Hooks::FILTER_FLUSH_HOOKS, self::FLUSH_CACHE_HOOKS) as $hook => $priority) {
             add_action($hook, [$this, 'flushCacheOnce'], $priority, 0);
         }
-
-        // Add action to flush entire cache whenever Autoptimize's cache is purged.
-        add_action('autoptimize_action_cachepurged', [$this, 'flushCacheOnce'], 10, 0);
 
         // Add action to flush entire cache manually with do_action().
         add_action(Hooks::ACTION_FLUSH_CACHE, [$this, 'flushCacheOnce'], 10, 0);

commit f1169f0156c3e2d9da14d61b725b477fc10597ee

Fix regression introduced in 2ea17756c58e34296d01fb502845efec901ab7ae

classes/BlueChip/Cache/Integrations.php
@@ -3,7 +3,7 @@
 namespace BlueChip\Cache;
 
 /**
- * Container for integrations with 3rt party plugins.
+ * Container for integrations with 3rd party plugins.
  */
 abstract class Integrations
 {
@@ -11,7 +11,7 @@ abstract class Integrations
     {
         // Integration with Autoptimize
         if (defined('AUTOPTIMIZE_PLUGIN_VERSION')) {
-            add_filter(Hooks::FILTER_FLUSH_HOOKS, [self::class, 'flushOnAutoptimizePurge'], 10, 0);
+            add_filter(Hooks::FILTER_FLUSH_HOOKS, [self::class, 'flushOnAutoptimizePurge'], 10, 1);
         }
 
         // Integration with Yoast SEO