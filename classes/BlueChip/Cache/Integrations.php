348c666d3255afc6109547c696ff0603d7817609
Česlav Przywara <cp@bluechip.at>
2021-11-16 13:56:58 +0100

    Tell Autoptimize to not warn about missing page caching plugin
    Fixes #64.

--- classes/BlueChip/Cache/Integrations.php
+++ classes/BlueChip/Cache/Integrations.php
@@ -11,7 +11,10 @@ abstract class Integrations
     {
         // Integration with Autoptimize
         if (\defined('AUTOPTIMIZE_PLUGIN_VERSION')) {
+            // Flush our caches when Autoptimize purges its caches.
             add_filter(Hooks::FILTER_FLUSH_HOOKS, [self::class, 'flushOnAutoptimizePurge'], 10, 1);
+            // Do not warn about missing page caching plugin ;-)
+            add_filter('autoptimize_filter_main_show_pagecache_notice', '__return_false', 10, 0);
         }
 
         // Integration with The SEO Framework
