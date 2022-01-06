9a7d62df1f29d921fd9c514539349bee55426f99
Česlav Przywara <cp@bluechip.at>
2022-01-06 14:58:06 +0100

    Install brain/monkey and set up PHPUnit

---
+++ tests/unit/src/TestCase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BlueChip\Cache\Tests\Unit;
+
+use Brain\Monkey;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    // See: https://github.com/Brain-WP/BrainMonkey/issues/39
+    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Monkey\setUp();
+
+        // Mock some more WordPress functions
+        // https://brain-wp.github.io/BrainMonkey/docs/functions-stubs.html
+
+        // Functions that always return their first argument.
+        Monkey\Functions\stubs(
+            [
+                '__', // https://github.com/Brain-WP/BrainMonkey/issues/25
+            ]
+        );
+
+        // Functions that always return false.
+        Monkey\Functions\stubs(
+            [
+                'is_multisite',
+            ],
+            false
+        );
+    }
+
+
+    protected function tearDown(): void
+    {
+        Monkey\tearDown();
+        parent::tearDown();
+    }
+}
