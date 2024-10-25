cca5ce7bab721a2b800a63f4ad2df5513bb66e1d
Česlav Przywara <cp@bluechip.at>
2024-10-25 20:02:11 +0200

    Refactor admin bar functionality into own class
    See #115.

--- classes/BlueChip/Cache/Plugin.php
+++ classes/BlueChip/Cache/Plugin.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace BlueChip\Cache;
 
-use function WP_CLI\Utils\wp_version_compare;
-
 class Plugin
 {
     /**
@@ -37,11 +35,6 @@ class Plugin
      * @var string Default value of cookie to denote front-end users.
      */
     private const FRONTEND_USER_COOKIE_VALUE = 'true';
-
-    /**
-     * @var string Name of nonce used for AJAX-ified flush cache requests.
-     */
-    private const NONCE_FLUSH_CACHE_REQUEST = 'bc-cache/nonce:flush-cache-request';
 
     /**
      * @var string Name of transient used to keep cache age and size information.
@@ -129,9 +122,14 @@ class Plugin
     private ?Feeder $cache_feeder = null;
 
     /**
-     * @var Viewer Viewer instance (initialized only in admin context).
+     * @var Viewer Viewer instance
      */
     private Viewer $cache_viewer;
+
+    /**
+     * @var AdminBar Admin bar instance
+     */
+    private AdminBar $admin_bar;
 
     /**
      * @var bool|null Null if cache has not been flushed yet in this request or cache flush status.
@@ -214,6 +212,8 @@ class Plugin
             $this->cache_feeder = new Feeder($this->cache, $this->feeder_lock);
             $this->cache_crawler = new Crawler($this->cache_feeder);
         }
+        $this->cache_viewer = new Viewer($this->cache, $this->cache_crawler, $this->cache_feeder);
+        $this->admin_bar = new AdminBar($plugin_filename, $this->cache, $this->cache_feeder, $this->cache_crawler, $this->cache_viewer);
     }
 
 
@@ -226,9 +226,6 @@ class Plugin
     {
         // Register initialization method.
         add_action('init', $this->init(...), 10, 0);
-
-        // Register method handling AJAX call from admin bar icon (or elsewhere).
-        add_action('wp_ajax_bc_cache_flush_cache', $this->processFlushRequest(...), 10, 0);
 
         // Integrate with WP-CLI.
         add_action('cli_init', function () {
@@ -272,15 +269,11 @@ class Plugin
         // Add action to flush entire cache manually with do_action().
         add_action(Hooks::ACTION_FLUSH_CACHE, $this->flushCacheOnce(...), 10, 0);
 
-        // Add toolbar to admin bar.
-        if (is_admin_bar_showing()) {
-            add_action('admin_bar_init', $this->enqueueToolbarAssets(...), 10, 0);
-            add_action('admin_bar_menu', $this->addToolbar(...), 110, 1);
-        }
+        // Initialize admin bar hooks.
+        $this->admin_bar->init();
 
         if (is_admin()) {
             // Initialize cache viewer.
-            $this->cache_viewer = new Viewer($this->cache, $this->cache_crawler, $this->cache_feeder);
             $this->cache_viewer->init();
 
             if (Utils::canUserFlushCache()) {
@@ -369,127 +362,6 @@ class Plugin
 
 
     /**
-     * @action https://developer.wordpress.org/reference/hooks/admin_bar_init/
-     */
-    private function enqueueToolbarAssets(): void
-    {
-        wp_enqueue_style(
-            'bc-cache-toolbar',
-            plugins_url('assets/toolbar.css', $this->plugin_filename),
-            [],
-            '20181201',
-            'all'
-        );
-
-        if (Utils::canUserFlushCache()) {
-            wp_enqueue_script(
-                'bc-cache-toolbar',
-                plugins_url('assets/toolbar.js', $this->plugin_filename),
-                ['jquery'],
-                '20190731',
-                true
-            );
-
-            wp_localize_script(
-                'bc-cache-toolbar',
-                'bc_cache_ajax_object',
-                [
-                    'ajaxurl' => admin_url('admin-ajax.php'), // necessary for the AJAX work properly on the frontend
-                    'nonce' => wp_create_nonce(self::NONCE_FLUSH_CACHE_REQUEST),
-                    'empty_cache_text' => __('Empty cache', 'bc-cache'),
-                    'warm_up_reset_text' => __('Warm-up status unavailable.', 'bc-cache'),
-                    'zero_age_text' => human_time_diff(\time()),
-                    'zero_size_text' => size_format(0),
-                ]
-            );
-        }
-    }
-
-
-    /**
-     * @action https://developer.wordpress.org/reference/hooks/admin_bar_menu/
-     *
-     * @param \WP_Admin_Bar $wp_admin_bar
-     */
-    private function addToolbar(\WP_Admin_Bar $wp_admin_bar): void
-    {
-        $size = $this->cache->getSize();
-        $cache_size = \is_int($size) ? (string)size_format($size) : __('unknown', 'bc-cache');
-
-        $age = $this->cache->getAge();
-        $cache_age = \is_int($age) ? human_time_diff($age) : __('unknown', 'bc-cache');
-
-        // If warm-up is enabled, add info about warm-up progress/status to the toolbar as well.
-        if ($this->cache_crawler && $this->cache_feeder) {
-            switch ($this->cache_crawler->getState()) {
-                case CrawlingState::FINISHED:
-                    $warm_up_class = 'bc-cache-warm-up-finished';
-                    $warm_up_status = __('Website is fully cached.', 'bc-cache');
-                    break;
-                case CrawlingState::RUNNING:
-                    $warm_up_class = 'bc-cache-warm-up-runs';
-                    $warm_up_status = \sprintf(
-                        __('Warm-up in progress (%d%%)', 'bc-cache'),
-                        $this->cache_feeder->getProgress(),
-                    );
-                    break;
-                case CrawlingState::SCHEDULED:
-                    $warm_up_class = 'bc-cache-warm-up-runs';
-                    $warm_up_status = \sprintf(
-                        __('Warm-up starts in %s.', 'bc-cache'),
-                        // Note: fallback value is not necessary here, but makes PHPStan happy.
-                        human_time_diff($this->cache_crawler->getNextScheduled() ?? 0),
-                    );
-                    break;
-                case CrawlingState::STALLED:
-                    $warm_up_class = 'bc-cache-warm-up-stalled';
-                    $warm_up_status = \sprintf(
-                        __('Warm-up stalled at %d%%.', 'bc-cache'),
-                        $this->cache_feeder->getProgress(),
-                    );
-                    break;
-            }
-        } else {
-            $warm_up_class = 'bc-cache-warm-up-off';
-            $warm_up_status = '';
-        }
-
-        // Add main BC Cache node.
-        $wp_admin_bar->add_node([
-            'id'    => 'bc-cache',
-            // 'parent' => 'top-secondary',
-            'title' => '<span class="ab-icon dashicons-html"></span><span class="ab-label">' . esc_html__('BC Cache', 'bc-cache') . '</span>',
-            'href'  => current_user_can(Viewer::REQUIRED_CAPABILITY) ? $this->cache_viewer->getUrl() : '',
-            'meta'  => [
-                'class' => $warm_up_class,
-            ]
-        ]);
-
-        // Add "stats" node.
-        $wp_admin_bar->add_node([
-            'id'        => 'bc-cache-stats',
-            'title'     => '<table>'
-                         . '<tr><th colspan="2">' . esc_html__('BC Cache Info', 'bc-cache') . '</th></tr>'
-                         . (($warm_up_status !== '') ? ('<tr><td colspan="2" class="bc-cache-warm-up-status" id="bc-cache-toolbar-warm-up-status">' . esc_html($warm_up_status) . '</td></tr>') : '')
-                         . '<tr><td>' . esc_html__('Size:', 'bc-cache') . '</td><td id="bc-cache-toolbar-cache-size">' . esc_html($cache_size) . '</td></tr>'
-                         . '<tr><td>' . esc_html__('Age:', 'bc-cache') . '</td><td id="bc-cache-toolbar-cache-age">' . esc_html($cache_age) . '</td></tr>'
-                         . '</table>'
-            ,
-            'parent'    => 'bc-cache',
-        ]);
-
-        // Add "clear cache" node, but only if current user can flush cache.
-        if (Utils::canUserFlushCache()) {
-            $wp_admin_bar->add_node([
-                'id'        => 'bc-cache-clear-button',
-                'title'     => '<button id="bc-cache-clear"><span class="ab-icon dashicons-trash"><span class="bc-cache-spinner"></span></span><span class="ab-label">' . esc_html__('Clear cache', 'bc-cache') . '</span></button>',
-                'parent'    => 'bc-cache',
-            ]);
-        }
-    }
-
-
-    /**
      * Add info about cache size to "At a Glance" box on dashboard. The snippet is linked to cache viewer page.
      *
      * @filter https://developer.wordpress.org/reference/hooks/dashboard_glance_items/
@@ -558,25 +430,6 @@ class Plugin
     private function flushCacheOnce(): void
     {
         $this->cache_is_flushed ??= $this->cache->flush();
-    }
-
-
-    /**
-     * Process AJAX flush request.
-     *
-     * @internal Is executed in context of AJAX request.
-     */
-    private function processFlushRequest(): void
-    {
-        // Check AJAX referer - die if invalid.
-        check_ajax_referer(self::NONCE_FLUSH_CACHE_REQUEST, false, true);
-
-        // TODO: in case of failure, indicate whether it's been access rights or I/O error.
-        if (Utils::canUserFlushCache() && $this->cache->flush()) {
-            wp_send_json_success();
-        } else {
-            wp_send_json_error();
-        }
     }
 
 
